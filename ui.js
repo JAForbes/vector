4512052ab64320c21fe7c50fb994612818b6892c
James <james.a.forbes@gmail.com>
2015-06-07 13:30:51 +1000

    separate text input handler from colour input and range input

--- ui.js
+++ ui.js
@@ -4,7 +4,42 @@ var ui = {
         state_list.addEventListener("click", this.removeStateType)
         can.addEventListener("mousewheel", this.changeStateType)
         btn_add_state_type.addEventListener("click", this.addStateElement.bind(null, null, null, null))
-        state_list.addEventListener('input', this.onInput)
+        state_list.addEventListener('input', this.onInput.bind(this))
+    },
+
+    onTextInput: function(event){
+        var input = event.srcElement
+        var li = input.parentElement
+        var last_li = state_list.childNodes[state_list.childNodes.length-1]
+        var second_last_li =state_list.childNodes[state_list.childNodes.length-2]
+        var isEmpty = input.value.length == 0
+
+        if(li == last_li){
+            if(!isEmpty){
+              addStateElement()
+            }
+        }
+
+        var prev = li.getAttribute("data-previous")
+
+        //code behind
+        if(!isEmpty){
+            //update key
+            state_types[input.value] = state_types[prev] || clone( state_types["default"] )
+            delete state_types[prev]
+
+            //todo-james not very performance friendly, but we're optimizing for rendering not input change
+
+            //update style state keys
+            for(var key in states){
+                var val = states[key]
+                if( val == prev) {
+                    states[key] = input.value
+                }
+            }
+
+            li.setAttribute("data-previous", input.value)
+        }
     },
 
     onInput: function(event){
@@ -12,40 +47,9 @@ var ui = {
 
         var li = input.parentElement
         if(input.type == "text") {
-
-            var li = input.parentElement
-            var last_li = state_list.childNodes[state_list.childNodes.length-1]
-            var second_last_li =state_list.childNodes[state_list.childNodes.length-2]
-            var isEmpty = input.value.length == 0
-
-            if(li == last_li){
-                if(!isEmpty){
-                  addStateElement()
-                }
-            }
-
-
-            var prev = li.getAttribute("data-previous")
-            //code behind
-            if(!isEmpty){
-                //update key
-                state_types[input.value] = state_types[prev] || clone( state_types["default"] )
-                delete state_types[prev]
-
-                //todo-james not very performance friendly, but we're optimizing for rendering not input change
-
-                //update style state keys
-                for(var key in states){
-                    var val = states[key]
-                    if( val == prev) {
-                        states[key] = input.value
-                    }
-                }
-
-                li.setAttribute("data-previous", input.value)
-            }
-
+           this.onTextInput(event)
         } else {
+            //colour and stroke width
             var key = li.childNodes[1].value
             var state_type = state_types[key]
             state_type.fillStyle = state_type.strokeStyle = li.childNodes[3].value
